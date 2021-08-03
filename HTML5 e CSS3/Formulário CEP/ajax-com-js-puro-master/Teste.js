9f305f9da3a9a14fd63d528b3eb832240f4f6d47
Thiago Aucelírio da Cunha <83706510+thiagocunha88@users.noreply.github.com>
2021-08-03 11:38:10 -0300

    HTML CSS básico, jogo, formulário e acessibilidade

---
+++ HTML5 e CSS3/Formulário CEP/ajax-com-js-puro-master/Teste.js
@@ -0,0 +1,21 @@
+let cep = '89237-153';
+
+function buscaCep() { 
+    let inputCep = document.querySelector('input\[name=cep\]'); 
+    let cep = inputCep.value.replace('-', '');
+    let url = 'http://viacep.com.br/ws/' + cep + '/json'; 
+    let xhr = new XMLHttpRequest(); xhr.open('GET', url, true); 
+    xhr.onreadystatechange = function() { 
+    if (xhr.readyState == 4) { 
+    if (xhr.status = 200) preencheCampos(JSON.parse(xhr.responseText)); } } 
+    xhr.send(); }
+console.log(JSON.parse(xhr.responseText));
+
+function preencheCampos(json) { 
+    document.querySelector('input[name=endereco]').value = json.logradouro;
+    document.querySelector('input[name=bairro]').value = json.bairro; 
+    document.querySelector('input[name=complemento]').value = json.complemento;
+    document.querySelector('input[name=cidade]').value = json.localidade; 
+    document.querySelector('input[name=estado]').value = json.uf; }
+
+    console.log(JSON.parse(xhr.responseText));
