c06ac7ea97a602298b89d54edad2ab801419233d
eraldosiniciof <eraldosinicio@gmail.com>
2021-10-18 06:21:15 -0300

    fix: confirm registration page

--- JavaScript/JS_na_web_-_CRUD_com_JavaScript_assincrono/admin/controller/cadastraCliente-controller.js
+++ JavaScript/JS_na_web_-_CRUD_com_JavaScript_assincrono/admin/controller/cadastraCliente-controller.js
@@ -3,10 +3,10 @@ import { clienteService } from "../service/cliente-service.js";
 const formulario = document.querySelector('[data-form]')
 
 formulario.addEventListener('submit', (evento) => {
-    evento.preventDefault
+    evento.preventDefault()
     const nome = evento.target.querySelector('[data-nome]').value
     const email = evento.target.querySelector('[data-email]').value
-    
+
     clienteService.criaCliente(nome, email)
     .then(()=> {
         console.log('passei aqui')
